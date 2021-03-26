230417ff9d9cd66b4e982570696bb353ad8428dc
Caio Zapparolli <52503208+czapparolli@users.noreply.github.com>
2021-03-25 22:11:40 -0300

    PROBLEMA: 1001 - Extremamente Básico
    RESPOSTA: Accepted

---
+++ Uri1001.java
@@ -0,0 +1,19 @@
+package UriGitHub;
+
+import java.util.Scanner;
+
+public class Uri1001 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		int x, y;
+
+		x = input.nextInt();
+		y = input.nextInt();
+
+		System.out.printf("X = %d\n", x + y);
+
+	}
+
+}
